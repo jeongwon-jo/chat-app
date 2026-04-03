9a94484284747bfb756c1620b4fa258421524022
jeongwon-jo <jjeong1.jo@gmail.com>
2026-04-03 11:33:05 +0900

    init db, auth setting

---
+++ app/api/auth/[...nextAuth]/route.ts
@@ -0,0 +1,78 @@
+import NextAuth, { AuthOptions } from "next-auth"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/libs/prismadb";
+import bcrypt from "bcrypt";
+
+export const authOptions: AuthOptions = {
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+		}),
+		GithubProvider({
+			clientId: process.env.GITHUB_CLIENT_ID!,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+		}),
+		CredentialsProvider({
+			name: "Credentials",
+			credentials: {
+				email: { label: "Email", type: "text", placeholder: "Email" },
+				password: { label: "Password", type: "Password" },
+			},
+			async authorize(credentials, req) {
+				if (!credentials?.email || !credentials?.password) {
+					throw new Error("Invalid Credentials");
+				}
+
+				const user = await prisma.user.findUnique({
+					where: {
+						email: credentials.email,
+					},
+				});
+
+				if (!user || !user?.hashedPassword) {
+					throw new Error("Invalid Credentials");
+				}
+
+				const isCorrectPassword = await bcrypt.compare(
+					credentials.password,
+					user?.hashedPassword,
+				);
+
+				if (!isCorrectPassword) {
+					throw new Error("Invalid Credentials");
+				}
+
+				return user;
+			},
+		}),
+	],
+	debug: process.env.NODE_ENV === "development",
+	session: {
+		strategy: "jwt",
+	},
+	secret: process.env.NEXTAUTH_SECRET,
+	jwt: {
+		secret: process.env.JWT_SECRET,
+		maxAge: 30 * 24 * 60 * 60,
+	},
+	pages: {
+		signIn: "/auth/login",
+	},
+	callbacks: {
+		async jwt({ token, user }) {
+			return { ...token, ...user };
+		},
+		async session({ session, token }) {
+			session.user = token;
+			return session;
+		},
+	},
+};
+
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
