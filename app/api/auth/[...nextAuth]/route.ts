b29ecb1a32bc001c7f569224c691955a2d6543a1
jeongwon-jo <129711334+jeongwon-jo@users.noreply.github.com>
2026-04-10 16:31:33 +0900

    fix

--- app/api/auth/[...nextAuth]/route.ts
+++ app/api/auth/[...nextAuth]/route.ts
@@ -1,7 +1,15 @@
-import NextAuth from "next-auth"
+import { NextRequest } from "next/server";
+import NextAuth from "next-auth";
 import { authOptions } from "@/libs/authOptions";
 
 export { authOptions };
 
-const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST }
+const handler = NextAuth(authOptions);
+
+export async function GET(req: NextRequest, ctx: { params: Promise<{ nextauth: string[] }> }) {
+  return handler(req, ctx);
+}
+
+export async function POST(req: NextRequest, ctx: { params: Promise<{ nextauth: string[] }> }) {
+  return handler(req, ctx);
+}
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	serverExternalPackages: ["next-auth", "@next-auth/prisma-adapter"],
 	images: {
 		remotePatterns: [
 			{
