e95dd6f4ac68f7441747c4edd22cb8616e2a6d5e
jeongwon-jo <129711334+jeongwon-jo@users.noreply.github.com>
2026-04-10 16:38:00 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	serverExternalPackages: ["next-auth", "@next-auth/prisma-adapter"],
+	serverExternalPackages: ["@next-auth/prisma-adapter"],
 	images: {
 		remotePatterns: [
 			{
